99963cedb1553c60421ec426144b02a01dd72706
Elek, Márton <level2@anix.net>
2014-11-01 21:01:09 +0100

    fix(db): fix sql syntax in migration script

--- model/src/main/resources/db/migration/V1_6__FieldFixes.sql
+++ model/src/main/resources/db/migration/V1_6__FieldFixes.sql
@@ -2,9 +2,9 @@ alter table mix modify column content longtext null;
 
 alter table textcontent modify column author varchar(255) null default '';
 
-alter table textcontent modify column created datetime default CURRENT_TIMESTAMP;
+alter table textcontent modify column created timestamp default CURRENT_TIMESTAMP;
 
-alter table textcontent modify column modified datetime default CURRENT_TIMESTAMP;
+alter table textcontent modify column modified timestamp default CURRENT_TIMESTAMP;
 
 alter table textcontent modify column content longtext null;
 
