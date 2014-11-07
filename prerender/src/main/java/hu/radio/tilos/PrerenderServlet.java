3f1600c1d44694b29e0503f6cc974c98076fe369
Elek, Márton <level2@anix.net>
2014-11-07 21:00:42 +0100

    chore(maven): rename root project

--- prerender/src/main/java/hu/radio/tilos/PrerenderServlet.java
+++ prerender/src/main/java/hu/radio/tilos/PrerenderServlet.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.nio.charset.Charset;
 
+@WebServlet(urlPatterns = "/prerender")
 public class PrerenderServlet extends HttpServlet {
 
     private String workDir = "/tmp/prerender";
