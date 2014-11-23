6921abe13eb473c66182e7ae2f317f25e19f9764
Elek, Márton <level2@anix.net>
2014-11-23 09:49:58 +0100

    fix(test): configuration handling for StatController test

--- service/src/test/java/hu/tilos/radio/backend/controller/StatControllerTest.java
+++ service/src/test/java/hu/tilos/radio/backend/controller/StatControllerTest.java
@@ -1,9 +1,12 @@
 package hu.tilos.radio.backend.controller;
 
+import hu.tilos.radio.backend.ConfigurationProducer;
+import hu.tilos.radio.backend.TestConfigProvider;
 import hu.tilos.radio.backend.TestUtil;
 import hu.tilos.radio.backend.controller.StatController;
 import hu.tilos.radio.backend.converters.MappingFactory;
 import hu.tilos.radio.backend.data.output.StatData;
+import org.jglue.cdiunit.ActivatedAlternatives;
 import org.jglue.cdiunit.AdditionalClasses;
 import org.jglue.cdiunit.CdiRunner;
 import org.junit.Assert;
@@ -14,7 +17,8 @@ import org.junit.runner.RunWith;
 import javax.inject.Inject;
 
 @RunWith(CdiRunner.class)
-@AdditionalClasses({MappingFactory.class, TestUtil.class})
+@AdditionalClasses({MappingFactory.class, TestUtil.class, ConfigurationProducer.class})
+@ActivatedAlternatives(TestConfigProvider.class)
 public class StatControllerTest {
 
 
