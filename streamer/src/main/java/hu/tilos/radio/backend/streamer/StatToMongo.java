6eca84675dc6ec8c72d05f3601e3cdcc4a55ba01
Elek, Marton <level2@anzix.net>
2015-02-22 17:24:02 +0100

    enh: save calculated start and end time

--- streamer/src/main/java/hu/tilos/radio/backend/streamer/StatToMongo.java
+++ streamer/src/main/java/hu/tilos/radio/backend/streamer/StatToMongo.java
@@ -12,11 +12,13 @@ import java.util.Date;
 
 public class StatToMongo {
 
+    private double SECOND_TO_BYTE = 38.28125 * 836;
+
     @Inject
     DB db;
 
     public String startDownload(Date startDate) {
-        BasicDBObject newRecord = new BasicDBObject("startDate", startDate);
+        BasicDBObject newRecord = new BasicDBObject("startDate", startDate).append("time", new Date()).append("realStartDate", startDate);
         WriteResult result = db.getCollection("stat_download").insert(newRecord);
         return "" + newRecord.getString("_id");
     }
@@ -25,12 +27,16 @@ public class StatToMongo {
         BasicDBObject query = new BasicDBObject("_id", new ObjectId(token));
         DBObject statRecord = db.getCollection("stat_download").findOne(query);
         statRecord.put("position", position);
+        Date startDate = (Date) statRecord.get("startDate");
+        statRecord.put("realStartDate", new Date(startDate.getTime() + Math.round((position / SECOND_TO_BYTE) * 1000)));
         db.getCollection("stat_download").update(query, statRecord);
     }
 
     public void endDownload(String token, int writtenBytes) {
         BasicDBObject query = new BasicDBObject("_id", new ObjectId(token));
         DBObject statRecord = db.getCollection("stat_download").findOne(query);
+        Date startDate = (Date) statRecord.get("startDate");
+        Date endDate = new Date(startDate.getTime() + Math.round((writtenBytes / SECOND_TO_BYTE) * 1000));
         statRecord.put("bytes", writtenBytes);
         db.getCollection("stat_download").update(query, statRecord);
 
