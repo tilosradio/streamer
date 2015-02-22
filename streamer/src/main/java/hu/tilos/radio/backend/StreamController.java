13d0d5a3644339e88de96247e4dda474c6e3fa68
Elek, Marton <level2@anzix.net>
2015-02-22 17:31:36 +0100

    enh: use interface for stat mongo + fix end date saving

--- streamer/src/main/java/hu/tilos/radio/backend/StreamController.java
+++ streamer/src/main/java/hu/tilos/radio/backend/StreamController.java
@@ -1,7 +1,7 @@
 package hu.tilos.radio.backend;
 
 import hu.tilos.radio.backend.streamer.Backend;
-import hu.tilos.radio.backend.streamer.StatToMongo;
+import hu.tilos.radio.backend.streamer.StatPersistence;
 import hu.tilos.radio.backend.streamer.util.Mp3Joiner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,7 +39,7 @@ public class StreamController extends HttpServlet {
     Backend backend;
 
     @Inject
-    StatToMongo stat;
+    StatPersistence stat;
 
     @Inject
     @Configuration(name = "server.url")
--- streamer/src/main/java/hu/tilos/radio/backend/streamer/StatToMongo.java
+++ streamer/src/main/java/hu/tilos/radio/backend/streamer/StatToMongo.java
@@ -10,19 +10,21 @@ import org.bson.types.ObjectId;
 import javax.inject.Inject;
 import java.util.Date;
 
-public class StatToMongo {
+public class StatToMongo implements StatPersistence {
 
     private double SECOND_TO_BYTE = 38.28125 * 836;
 
     @Inject
     DB db;
 
+    @Override
     public String startDownload(Date startDate) {
         BasicDBObject newRecord = new BasicDBObject("startDate", startDate).append("time", new Date()).append("realStartDate", startDate);
         WriteResult result = db.getCollection("stat_download").insert(newRecord);
         return "" + newRecord.getString("_id");
     }
 
+    @Override
     public void seek(String token, int position) {
         BasicDBObject query = new BasicDBObject("_id", new ObjectId(token));
         DBObject statRecord = db.getCollection("stat_download").findOne(query);
@@ -32,12 +34,14 @@ public class StatToMongo {
         db.getCollection("stat_download").update(query, statRecord);
     }
 
+    @Override
     public void endDownload(String token, int writtenBytes) {
         BasicDBObject query = new BasicDBObject("_id", new ObjectId(token));
         DBObject statRecord = db.getCollection("stat_download").findOne(query);
         Date startDate = (Date) statRecord.get("startDate");
         Date endDate = new Date(startDate.getTime() + Math.round((writtenBytes / SECOND_TO_BYTE) * 1000));
         statRecord.put("bytes", writtenBytes);
+        statRecord.put("endDate", endDate);
         db.getCollection("stat_download").update(query, statRecord);
 
     }
