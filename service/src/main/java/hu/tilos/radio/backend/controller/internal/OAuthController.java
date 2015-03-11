56aec68496f6f978617d0a7359bdb1c3f7f87631
Elek, Márton <level2@anix.net>
2015-03-11 22:15:18 +0100

    imp: make facebook login configurable

--- service/src/main/java/hu/tilos/radio/backend/controller/internal/OAuthController.java
+++ service/src/main/java/hu/tilos/radio/backend/controller/internal/OAuthController.java
@@ -7,6 +7,7 @@ import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
 import com.restfb.types.User;
 import hu.radio.tilos.model.Role;
+import hu.tilos.radio.backend.Configuration;
 import hu.tilos.radio.backend.Security;
 import hu.tilos.radio.backend.Session;
 import hu.tilos.radio.backend.data.Token;
@@ -28,9 +29,6 @@ import java.util.Map;
 import java.util.Scanner;
 
 
-/**
- * Generate atom feed for the shows.
- */
 @Path("/api/int/oauth")
 public class OAuthController {
 
@@ -45,6 +43,14 @@ public class OAuthController {
     @Inject
     private JWTEncoder jwtEncoder;
 
+    @Inject
+    @Configuration(name = "facebook.key")
+    private String clientId;
+
+    @Inject
+    @Configuration(name = "facebook.secret")
+    private String clientKey;
+
     /**
      * @exclude
      */
@@ -55,50 +61,58 @@ public class OAuthController {
     @Path("/facebook")
     public Response facebook(FacebookRequest request) {
         try {
-            LOG.info("Checking facebook authetnication of code: " + request.getCode());
-            Map result = new HashMap<>();
-            //change to an access token
-            String clientId = "1390285161277354";
-            String clientKey = "xxx";
-            String url = String.format("https://graph.facebook.com/oauth/access_token?client_id=%s&client_secret=%s&code=%s&redirect_uri=%s", clientId, clientKey, request.code, "http://tiloslocal.hu/");
-            System.out.println(url);
-            URLConnection uc = new URL(url).openConnection();
-            String response = new Scanner(uc.getInputStream()).useDelimiter("\\Z").next();
-            System.out.println(response);
-            String[] parts = response.split("&");
 
-            FacebookClient facebookClient = new DefaultFacebookClient(parts[0].split("=")[1]);
+            String accessToken = getAccessToken(request.code);
 
-            User me = facebookClient.fetchObject("me", User.class);
+            DBObject userResponse = saveOrGetUser(accessToken);
 
+            Map result = createToken(userResponse);
 
-            DBObject userResponse = db.getCollection("user").findOne(new BasicDBObject("facebook", me.getId()));
-            if (userResponse == null) {
-                BasicDBObject newUser = new BasicDBObject()
-                        .append("facebook", me.getId())
-                        .append("username", me.getId() + "@facebook.com")
-                        .append("email", me.getEmail())
-                        .append("role_id", Role.USER.ordinal());
-                db.getCollection("user").insert(newUser);
-                userResponse = db.getCollection("user").findOne(new BasicDBObject("facebook", me.getId()));
-            }
-
-            Token jwtToken = new Token();
-            jwtToken.setUsername((String) userResponse.get("username"));
-            jwtToken.setRole(Role.USER);
-            result.put("token", jwtEncoder.encode(jwtToken));
             return Response.ok(result).build();
-
-
-            //check if user exists
-            //create if not
-            //save the user data
 
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    private Map createToken(DBObject userResponse) {
+        Map result = new HashMap<>();
+        Token jwtToken = new Token();
+        jwtToken.setUsername((String) userResponse.get("username"));
+        jwtToken.setRole(Role.USER);
+        result.put("token", jwtEncoder.encode(jwtToken));
+        return result;
+    }
+
+    private DBObject saveOrGetUser(String accessToken) {
+        FacebookClient facebookClient = new DefaultFacebookClient(accessToken);
+
+        User me = facebookClient.fetchObject("me", User.class);
+
+        DBObject userResponse = db.getCollection("user").findOne(new BasicDBObject("facebook", me.getId()));
+
+        if (userResponse == null) {
+            BasicDBObject newUser = new BasicDBObject()
+                    .append("facebook", me.getId())
+                    .append("username", me.getEmail())
+                    .append("email", me.getEmail())
+                    .append("role_id", Role.USER.ordinal());
+            db.getCollection("user").insert(newUser);
+            userResponse = db.getCollection("user").findOne(new BasicDBObject("facebook", me.getId()));
+        }
+        return userResponse;
+    }
+
+    private String getAccessToken(String code) throws IOException {
+        //change to an access token
+        String url = String.format("https://graph.facebook.com/oauth/access_token?client_id=%s&client_secret=%s&code=%s&redirect_uri=%s", clientId, clientKey, code, "http://tiloslocal.hu/");
+        URLConnection uc = new URL(url).openConnection();
+        String response = new Scanner(uc.getInputStream()).useDelimiter("\\Z").next();
+
+        String[] parts = response.split("&");
+        return parts[0].split("=")[1];
     }
 
     public static class FacebookRequest {
