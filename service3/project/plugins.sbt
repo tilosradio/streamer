028ac67758589b1597e5e56e0b952260df5085c5
Elek, Márton <level2@anix.net>
2015-03-31 08:50:59 +0200

    move service3 to separated repository

--- service3/project/plugins.sbt
+++
@@ -1,2 +0,0 @@
-addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.2")
-addSbtPlugin("com.typesafe.sbt" % "sbt-atmos" % "0.3.2")
--- service3/src/main/scala/hu/tilos/service3/UserObj.scala
+++
@@ -1,6 +0,0 @@
-package hu.tilos.service3
-import hu.tilos.service3.common.Role._
-
-case class UserObj(id: String, username: String, role: Role) {
-
-}
