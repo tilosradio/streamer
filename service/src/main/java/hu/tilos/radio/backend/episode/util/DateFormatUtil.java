5e1d8959e7721e65255b55c2e1455c09d0770573
Elek, Márton <level2@anix.net>
2015-06-23 22:27:11 +0200

    fix date format util

--- service/src/main/java/hu/tilos/radio/backend/episode/util/DateFormatUtil.java
+++ service/src/main/java/hu/tilos/radio/backend/episode/util/DateFormatUtil.java
@@ -11,11 +11,11 @@ public class DateFormatUtil {
 
     public static final SimpleDateFormat YYYY_MM_DD = create("yyyy-MM-dd");
 
-    public static final SimpleDateFormat YYYY_DOT_MM_DOT_DD = new SimpleDateFormat("yyyy'.'MM'.'dd", LocaleUtil.TILOSLOCALE);
+    public static final SimpleDateFormat YYYY_DOT_MM_DOT_DD = create("yyyy'.'MM'.'dd");
 
-    public static final SimpleDateFormat MM_DD = new SimpleDateFormat("MM'.'dd", LocaleUtil.TILOSLOCALE);
+    public static final SimpleDateFormat MM_DD = create("MM'.'dd");
 
-    public  static final SimpleDateFormat HH_MM = new SimpleDateFormat("HH':'mm", LocaleUtil.TILOSLOCALE);
+    public  static final SimpleDateFormat HH_MM = create("HH':'mm");
 
     public static SimpleDateFormat create(String pattern) {
         SimpleDateFormat format = new SimpleDateFormat(pattern, LocaleUtil.TILOSLOCALE);
