9a44eec4c299e9874b831c7806002b539fd7730e
Elek, Márton <level2@anix.net>
2015-08-09 12:17:51 +0200

    new: working version of the spray version of the rest api

--- service/src/main/scala/hu/tilos/service3/Endpoint.scala
+++ service/src/main/scala/hu/tilos/service3/Endpoint.scala
@@ -17,7 +17,7 @@ import spray.http.{BodyPart, MultipartFormData, StatusCodes}
 import spray.httpx.SprayJsonSupport
 import spray.json._
 import spray.routing._
-
+import StatusJsonSupport._
 import scala.concurrent.duration._
 
 
@@ -49,49 +49,62 @@ class MyServiceActor(implicit inj: Injector) extends Actor with HttpService with
 
   val myRoute =
 
-    path("api" / "v2" / "bookmark") {
+    logRequest("session reached", akka.event.Logging.InfoLevel) {
+      get {
+        path("api" / "v2" / "ping") {
+          respondWithMediaType(`application/json`) { requestContext =>
+            requestContext.complete(Ok("pong").toJson.prettyPrint)
+          }
+        } ~
+        path("api" / "v2" / "ping2") {
+          respondWithMediaType(`application/json`) { requestContext =>
+            requestContext.complete(Ok("pong2").toJson.prettyPrint)
+          }
+        }
+      }
+    }
+
+  path("api" / "v2" / "bookmark") {
+    post {
+      respondWithMediaType(`application/json`) {
+        authenticate(authenticator) { user =>
+          authorize(isInRole(user, Role.Author)) {
+            entity(as[BookmarkRequest]) { bookmark => requestContext =>
+              (bookmarkWorker ? BookmarkActor.Save(bookmark, user)).map {
+                case ok: Ok => requestContext.complete(ok.toJson.prettyPrint)
+                case error: BadRequest => {
+                  requestContext.complete(StatusCodes.BadRequest, error.message)
+                }
+                case _ => println("Unkown message")
+              }
+            }
+          }
+        }
+      }
+    }
+
+  } ~
+    path("api" / "v2" / "upload") {
       post {
         respondWithMediaType(`application/json`) {
           authenticate(authenticator) { user =>
             authorize(isInRole(user, Role.Author)) {
-              entity(as[BookmarkRequest]) { bookmark => requestContext =>
-                import StatusJsonSupport._
-                (bookmarkWorker ? BookmarkActor.Save(bookmark, user)).map {
-                  case ok: Ok => requestContext.complete(ok.toJson.prettyPrint)
-                  case error: BadRequest => {
-                    requestContext.complete(StatusCodes.BadRequest, error.message)
-                  }
-                  case _ => println("Unkown message")
-                }
-              }
-            }
-          }
-        }
-      }
-
-    } ~
-      path("api" / "v2" / "upload") {
-        post {
-          respondWithMediaType(`application/json`) {
-            authenticate(authenticator) { user =>
-              authorize(isInRole(user, Role.Author)) {
-                entity(as[MultipartFormData]) { formData =>
-                  detach() {
-                    complete {
-                      val details = formData.fields.map {
-                        case BodyPart(entity, headers) =>
-                          //val content = entity.buffer
-                          val content = new ByteArrayInputStream(entity.data.toByteArray)
-                          //                  val contentType = headers.find(h => h.is("content-type")).get.value
-                          val contentType = "text/plain"
-                          val fileName = headers.find(h => h.is("content-disposition")).get.value.split("filename=").last
-                          val result = saveAttachment(fileName, content)
-                          (contentType, fileName, result)
-                        case t: Throwable => println("Unhandles error message " + t.getMessage)
-                        case _ => println("Unhandles message")
-                      }
-                      s"""{"status": "Processed POST request, details=$details" }"""
+              entity(as[MultipartFormData]) { formData =>
+                detach() {
+                  complete {
+                    val details = formData.fields.map {
+                      case BodyPart(entity, headers) =>
+                        //val content = entity.buffer
+                        val content = new ByteArrayInputStream(entity.data.toByteArray)
+                        //                  val contentType = headers.find(h => h.is("content-type")).get.value
+                        val contentType = "text/plain"
+                        val fileName = headers.find(h => h.is("content-disposition")).get.value.split("filename=").last
+                        val result = saveAttachment(fileName, content)
+                        (contentType, fileName, result)
+                      case t: Throwable => println("Unhandles error message " + t.getMessage)
+                      case _ => println("Unhandles message")
                     }
+                    s"""{"status": "Processed POST request, details=$details" }"""
                   }
                 }
               }
@@ -99,6 +112,7 @@ class MyServiceActor(implicit inj: Injector) extends Actor with HttpService with
           }
         }
       }
+    }
 
   private def saveAttachment(fileName: String, content: InputStream): Boolean = {
     saveAttachment[InputStream](fileName, content, {
