ae51cd928d5f75de1c0483d038c1bfa95ee84776
Elek, Márton <level2@anix.net>
2015-10-15 22:19:22 +0200

    fix: force docker image tagging

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 docker build -t tilosradio/backend .
-docker tag tilosradio/backend localhost:5000/tilosradio/backend
+docker tag -f tilosradio/backend localhost:5000/tilosradio/backend
 docker push localhost:5000/tilosradio/backend
