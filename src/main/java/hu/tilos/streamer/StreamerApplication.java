d6b6cc641601d2035a2ffaecb0e18a80585fd10e
Elek, Márton <elek@users.noreply.github.com>
2017-03-16 23:22:24 +0100

    active session counter

--- src/main/java/hu/tilos/streamer/StreamerApplication.java
+++ src/main/java/hu/tilos/streamer/StreamerApplication.java
@@ -1,11 +1,16 @@
 package hu.tilos.streamer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.ResourceRegionHttpMessageConverter;
+
+import javax.servlet.*;
+import java.io.IOException;
 
 @SpringBootApplication
 @Configuration
@@ -18,5 +23,35 @@ public class StreamerApplication {
   @Bean
   public HttpMessageConverters customConverter() {
     return new HttpMessageConverters(new ResourceRegionHttpMessageConverter());
+  }
+
+  @Configuration
+  static class Config {
+    @Autowired
+    CounterService counterService;
+
+    @Bean
+    Filter counterFilter() {
+      return new Filter() {
+        @Override
+        public void init(FilterConfig filterConfig) throws ServletException {
+        }
+
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+          try {
+            counterService.increment("active-http-sessions");
+            filterChain.doFilter(servletRequest, servletResponse);
+          } finally {
+            counterService.decrement("active-http-sessions");
+          }
+        }
+
+        @Override
+        public void destroy() {
+        }
+      };
+    }
+
   }
 }
